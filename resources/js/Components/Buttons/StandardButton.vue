984db3e5777bc92bb2a7f9e3a175b742875003c3
Nadim Salloum <github@salloum.pm>
2026-02-13 22:23:45 +0100

    fix button classes

--- resources/js/Components/Buttons/StandardButton.vue
+++ resources/js/Components/Buttons/StandardButton.vue
@@ -14,8 +14,12 @@ export default {
   },
   computed: {
     allClasses() {
+      const colourClasses = {
+        gray: 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900 focus:ring-gray-300',
+        green: 'bg-green-800 hover:bg-green-700 active:bg-green-900 focus:border-green-900 focus:ring-green-300',
+      };
       let classes = 'justify-center inline-flex items-center sm:px-4 sm:py-2 px-2 py-1 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25 transition';
-      classes += ` bg-${this.colour}-800 hover:bg-${this.colour}-700 active:bg-${this.colour}-900 focus:border-${this.colour}-900 focus:ring-${this.colour}-300`;
+      classes += ` ${colourClasses[this.colour] || colourClasses.gray}`;
       return `${classes} ${this.class}`;
     },
   },
